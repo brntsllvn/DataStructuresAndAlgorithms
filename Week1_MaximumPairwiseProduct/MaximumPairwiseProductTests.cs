c8048ba39a61011743baf79f5f80d8475c9e5895
Brent Sullivan <brntsllvn@gmail.com>
2016-06-09 07:47:21 -0700

    implement and test max pairwise product brute force logic

---
+++ Week1_MaximumPairwiseProduct/MaximumPairwiseProductTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace Week1_MaximumPairwiseProduct
+{
+    [TestFixture]
+    class MaximumPairwiseProductTests
+    {
+        [Test]
+        public void Pairwise_Product1()
+        {
+            var arr = new int[] { 1, 2, 3 };
+            var product = new Launcher().FindMaxPairwiseProduct(arr);
+
+            product.ShouldBe(6);
+        }
+
+        [Test]
+        public void Pairwise_Product2()
+        {
+            var arr = new int[] { 7, 5, 14, 2, 8, 8, 10, 1, 2, 3 };
+            var product = new Launcher().FindMaxPairwiseProduct(arr);
+
+            product.ShouldBe(140);
+        }
+
+        [Test]
+        public void Pairwise_Product3()
+        {
+            var arr = new int[] { 4, 6, 2, 6, 1 };
+            var product = new Launcher().FindMaxPairwiseProduct(arr);
+
+            product.ShouldBe(36);
+        }
+    }
+}
--- Week1_MaximumPairwiseProduct/Program.cs
+++ Week1_MaximumPairwiseProduct/Program.cs
@@ -1,29 +1,93 @@
 ﻿using System;
-using NUnit.Framework;
-using Shouldly;
+using System.Linq;
 
 namespace Week1_MaximumPairwiseProduct
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("hello, world!");
-            Console.Read();
+            new Launcher().Run(args);
         }
     }
 
-    [TestFixture]
-    class ProgramTests
+    public class Launcher
     {
-        [Test]
-        public void Program_Sum_Works()
+        public void Run(string[] args)
         {
-            var a = 3;
-            var b = 17;
-            var sum = a + b;
+            int[] userInts;
 
-            sum.ShouldBe(20);
+            while (true)
+            {
+                Console.WriteLine("Number of integers: ");
+                var numOfInts = int.Parse(Console.ReadLine());
+                Console.WriteLine("Integers: ");
+                userInts = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+
+                if (!InputHasErrors(numOfInts, userInts))
+                    break;
+            }
+
+            Console.WriteLine(FindMaxPairwiseProduct(userInts));
+        }
+
+        public int FindMaxPairwiseProduct(int[] userInts)
+        {
+            if (userInts.Length == 1)
+            {
+                return userInts[0];
+            }
+
+            var maxPairwiseProduct = 0;
+            var length = userInts.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    Int32 product = userInts[i]*userInts[j];
+                    if (i != j && product > maxPairwiseProduct)
+                    {
+                        maxPairwiseProduct = product;
+                    }
+                }    
+            }
+
+            return maxPairwiseProduct;
+        }
+
+
+        private bool InputHasErrors(int numOfInts, int[] userInts)
+        {
+            if (numOfInts < 2 || numOfInts > 2 * 100000)
+            {
+                Console.WriteLine("Number of integers out of bounds");
+                return true;
+            }
+
+            if (userInts.Length != numOfInts)
+            {
+                Console.WriteLine("Number of integers incorrect");
+                return true;
+            }
+
+            foreach (var userInt in userInts)
+            {
+                if (userInt > 100000)
+                {
+                    Console.WriteLine("individual integer input too big");
+                    return true;
+                }
+
+
+                if (userInt < 0)
+                {
+                    Console.WriteLine("individual integer input less than zero");
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
