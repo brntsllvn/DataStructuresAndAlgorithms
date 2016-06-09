21ab84486efabd4ad0f8e8787f416821b470bb96
Brent Sullivan <brntsllvn@gmail.com>
2016-06-09 07:57:55 -0700

    Add launcher template to facilitate unit testing of static classes

---
+++ LauncherTemplate/LauncherTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace LauncherTemplate
+{
+    [TestFixture]
+    class LauncherTests
+    {
+        [Test]
+        public void Launcher_Works()
+        {
+            true.ShouldBe(true);
+        } 
+    }
+}
---
+++ LauncherTemplate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace LauncherTemplate
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Launcher().Run(args);
+        }
+    }
+
+    public class Launcher
+    {
+        public void Run(string[] args)
+        {
+
+        }
+    }
+}
