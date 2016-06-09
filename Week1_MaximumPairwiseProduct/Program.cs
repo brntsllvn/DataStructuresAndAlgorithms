44f35fcfc600d32165c2846d6b1bd9e0bdac4db0
Brent Sullivan <brntsllvn@gmail.com>
2016-06-09 06:23:31 -0700

    Add new project: Maximum Pairwise Product

---
+++ Week1_MaximumPairwiseProduct/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Week1_MaximumPairwiseProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello, world!");
+            Console.Read();
+        }
+    }
+
+    [TestFixture]
+    class ProgramTests
+    {
+        [Test]
+        public void Program_Sum_Works()
+        {
+            var a = 3;
+            var b = 17;
+            var sum = a + b;
+
+            sum.ShouldBe(20);
+        }
+    }
+}
