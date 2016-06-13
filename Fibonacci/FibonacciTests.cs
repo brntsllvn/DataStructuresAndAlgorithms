115f427578fb2c9df44e4f15409ebaee0aba8fb4
Brent Sullivan <brntsllvn@gmail.com>
2016-06-13 10:12:26 -0700

    faster solution

--- Fibonacci/FibonacciTests.cs
+++ Fibonacci/FibonacciTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using Shouldly;
 
 namespace Fibonacci
@@ -6,10 +7,23 @@ namespace Fibonacci
     [TestFixture]
     class FibonacciTests
     {
-        [Test]
-        public void Launcher_Works()
+        [TestCase(0,0)]
+        [TestCase(1,1)]
+        [TestCase(2,1)]
+        [TestCase(3,2)]
+        [TestCase(10,55)]
+        [TestCase(20,6765)]
+        [TestCase(50,12586269025)]
+        public void Fibonacci(long input, long expected)
         {
-            true.ShouldBe(false);
+            var f0 = new Launcher();
+            f0.CalculateFibonacci(input).ShouldBe(expected);
+
+            Should.CompleteIn(
+                () => f0.CalculateFibonacci(input), TimeSpan.FromMilliseconds(1500));
+
+            long memory = GC.GetTotalMemory(true);
+            memory.ShouldBeLessThanOrEqualTo(512*1000*1000);
         }
     }
 }
--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fibonacci
 {
@@ -18,7 +14,28 @@ namespace Fibonacci
     {
         public void Run(string[] args)
         {
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(CalculateFibonacci(n));
+        }
 
+        public long CalculateFibonacci(long n)
+        {
+            if (n == 0)
+                return 0;
+
+            if (n == 1)
+                return 1;
+
+            var fibonacciArray = new long[n];
+            fibonacciArray[0] = 0;
+            fibonacciArray[1] = 1;
+
+            for (long i = 2; i < n; i++)
+            {
+                fibonacciArray[i] = fibonacciArray[i-1] + fibonacciArray[i-2];
+            }
+
+            return fibonacciArray[n-1] + fibonacciArray[n-2];
         }
     }
 }
