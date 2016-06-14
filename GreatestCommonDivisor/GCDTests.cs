58dc615560b102bf9032a99e057634265aea9274
Brent Sullivan <brntsllvn@gmail.com>
2016-06-14 06:52:15 -0700

    Set up GCD problem

---
+++ GreatestCommonDivisor/GCDTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using Shouldly;
+namespace GreatestCommonDivisor
+{
+    [TestFixture]
+    class FibonacciTests
+    {
+        [TestCase(0, 1, 0)]
+        [TestCase(3, 5, 1)]
+        [TestCase(2, 2*1000*1000*1000, 2)]
+        public void Test_1(long[] inputLongs, long expected)
+        {
+            var f0 = new Launcher();
+            f0.CalculateGcd(inputLongs).ShouldBe(expected);
+
+            Should.CompleteIn(
+                () => f0.CalculateGcd(inputLongs), TimeSpan.FromMilliseconds(1500));
+
+            var proc = Process.GetCurrentProcess();
+            proc.PrivateMemorySize64.ShouldBeLessThanOrEqualTo(512 * 1000 * 1000);
+        }
+    }
+}
---
+++ GreatestCommonDivisor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace GreatestCommonDivisor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Launcher().Run(args);
+        }
+    }
+
+    public class Launcher
+    {
+        public void Run(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray(); 
+            Console.WriteLine(CalculateGcd(input));
+        }
+
+        public long CalculateGcd(long[] inputLongs)
+        {
+            return 0;
+        }
+    }
+}
