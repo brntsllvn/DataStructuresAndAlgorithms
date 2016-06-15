f48d5edb28e165e57296dd64ea66d11d4ee1ab45
Brent Sullivan <brntsllvn@gmail.com>
2016-06-15 06:22:45 -0700

    FibMod problem set up

---
+++ FibonacciMod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciMod
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Launcher().Run(args);
+        }
+    }
+
+    public class Launcher
+    {
+        public void Run(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
+            Console.WriteLine(MyFunction(input));
+        }
+
+        public long MyFunction(long[] n)
+        {
+            return 1;
+        }
+    }
+}
