c19a34ee5df07a1e33296ff93989ab5c79e1f9ff
Brent Sullivan <brntsllvn@gmail.com>
2016-06-14 18:02:32 -0700

    lcm solution

---
+++ LeastCommonMultiple/LCMTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using Shouldly;
+
+namespace LeastCommonMultiple
+{
+    [TestFixture]
+    class LcmTests
+    {
+        [TestCase(new long[] { 1, 1 }, 1)]
+        [TestCase(new long[] { 6, 8 }, 24)]
+        [TestCase(new long[] { 330, 65 }, 4290)]
+        [TestCase(new long[] { 1571, 97 }, 152387)]
+        [TestCase(new long[] { 28851538, 1183019 }, 1933053046)]
+        [TestCase(new long[] { 2, 2 * 1000 * 1000 * 1000 }, 2 * 1000 * 1000 * 1000)]
+        public void Test_1(long[] inputLongs, long expected)
+        {
+            var f0 = new Launcher();
+            f0.CalculateLcm(inputLongs).ShouldBe(expected);
+
+            Should.CompleteIn(
+                () => f0.CalculateLcm(inputLongs), TimeSpan.FromMilliseconds(1500));
+
+            var proc = Process.GetCurrentProcess();
+            proc.PrivateMemorySize64.ShouldBeLessThanOrEqualTo(512 * 1000 * 1000);
+        }
+    }
+}
---
+++ LeastCommonMultiple/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace LeastCommonMultiple
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Launcher().Run(args);
+        }
+    }
+
+    public class Launcher
+    {
+        public void Run(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
+            Console.WriteLine(CalculateLcm(input));
+        }
+
+        public long CalculateLcm(long[] inputLongs)
+        {
+            var Gcd = CalculateGcd(new long[] {inputLongs[0], inputLongs[1]});
+
+            return (long) ((double)(inputLongs[0] * inputLongs[1]) / (double)Gcd);
+        }
+
+        public long CalculateGcd(long[] inputLongs)
+        {
+            if (inputLongs[0] == 1 || inputLongs[1] == 1)
+                return 1;
+
+            if (inputLongs[0] == inputLongs[1])
+                return inputLongs[0];
+
+            var maxInput = inputLongs[0] > inputLongs[1] ? inputLongs[0] : inputLongs[1];
+            var minInput = inputLongs[0] > inputLongs[1] ? inputLongs[1] : inputLongs[0];
+
+            var remainder = maxInput % minInput;
+
+            if (remainder == 0)
+                return minInput;
+
+            return CalculateGcd(new long[] { remainder, minInput });
+        }
+    }
+}
