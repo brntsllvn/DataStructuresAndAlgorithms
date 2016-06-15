057d3d525b60861b804fd4be406423066bd289df
Brent Sullivan <brntsllvn@gmail.com>
2016-06-15 06:55:27 -0700

    Fib Mod base cases

--- FibonacciMod/FibonacciModTests.cs
+++ FibonacciMod/FibonacciModTests.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
 
 namespace FibonacciMod
 {
+    [TestFixture]
     class FibonacciModTests
     {
+        [TestCase(new long[] { 1, 2 }, 1)]
+        [TestCase(new long[] { 1, 3 }, 1)]
+        [TestCase(new long[] { 1, 8 }, 1)]
+        [TestCase(new long[] { 2, 2 }, 1)]
+        [TestCase(new long[] { 2, 3 }, 1)]
+        [TestCase(new long[] { 2, 8 }, 1)]
+        //[TestCase(new long[] { 2, 2 }, 1)]
+        //[TestCase(new long[] { 3, 2 }, 0)]
+        //[TestCase(new long[] { 4, 2 }, 1)]
+        //[TestCase(new long[] { 4, 3 }, 0)]
+        //[TestCase(new long[] { 5, 3 }, 2)]
+        //[TestCase(new long[] { 1 * 10, 2 * 1000 * 1000 * 1000 }, )]
+        public void Test_1(long[] inputLongs, long expected)
+        {
+            var f0 = new Launcher();
+            f0.CalculateFibonacciMod(inputLongs).ShouldBe(expected);
+
+            Should.CompleteIn(
+                () => f0.CalculateFibonacciMod(inputLongs), TimeSpan.FromMilliseconds(1500));
+
+            var proc = Process.GetCurrentProcess();
+            proc.PrivateMemorySize64.ShouldBeLessThanOrEqualTo(512 * 1000 * 1000);
+        }
     }
 }
--- FibonacciMod/Program.cs
+++ FibonacciMod/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FibonacciMod
 {
@@ -19,12 +16,44 @@ namespace FibonacciMod
         public void Run(string[] args)
         {
             var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
-            Console.WriteLine(MyFunction(input));
+            Console.WriteLine((input));
         }
 
-        public long MyFunction(long[] n)
+        public long CalculateFibonacciMod(long[] n)
         {
-            return 1;
+            var fibonacciTerm = n[0];
+            var mod = n[1];
+
+            if (fibonacciTerm == 1 || fibonacciTerm == 2)
+                return 1;
+
+            if (fibonacciTerm == 3)
+                return 2 % mod;
+
+
+
+                return 0;
         }
+
+        //public long CalculateFibonacci(long n)
+        //{
+        //    if (n == 0)
+        //        return 0;
+
+        //    if (n == 1)
+        //        return 1;
+
+        //    var fibonacciArray = new long[n];
+        //    fibonacciArray[0] = 0;
+        //    fibonacciArray[1] = 1;
+
+        //    for (long i = 2; i < n; i++)
+        //    {
+        //        fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2];
+        //    }
+
+        //    return fibonacciArray[n - 1] + fibonacciArray[n - 2];
+        //}
     }
+
 }
