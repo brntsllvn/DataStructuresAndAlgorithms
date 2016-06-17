766e7a87c0bdd6f3d46d44f5ea5ce5ec71abcf2f
=brntsllvn <=brntsllvn@gmail.com>
2016-06-17 08:14:40 -0700

    changing money solved

--- ChangingMoney/ChangingMoneyTests.cs
+++ ChangingMoney/ChangingMoneyTests.cs
@@ -8,14 +8,25 @@ namespace ChangingMoney
     [TestFixture]
     class CMTests
     {
-        [TestCase(new long[] { 0 }, "hello")]
-        public void Test_1(long[] input, string expected)
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        [TestCase(3, 3)]
+        [TestCase(4, 4)]
+        [TestCase(5, 1)]
+        [TestCase(6, 2)]
+        [TestCase(8, 4)]
+        [TestCase(10, 1)]
+        [TestCase(14, 5)]
+        [TestCase(15, 2)]
+        [TestCase(16, 3)]
+        [TestCase(28, 6)]
+        public void Test_1(int input, int expected)
         {
             var f0 = new Launcher();
-            f0.MyFunction(input).ShouldBe(expected);
+            f0.MinimumNumberOfCoinsToChangeMoney(input).ShouldBe(expected);
 
             Should.CompleteIn(
-                () => f0.MyFunction(input), TimeSpan.FromMilliseconds(1500));
+                () => f0.MinimumNumberOfCoinsToChangeMoney(input), TimeSpan.FromMilliseconds(1500));
 
             var proc = Process.GetCurrentProcess();
             proc.PrivateMemorySize64.ShouldBeLessThanOrEqualTo(512 * 1000 * 1000);
--- ChangingMoney/Program.cs
+++ ChangingMoney/Program.cs
@@ -15,13 +15,32 @@ namespace ChangingMoney
     {
         public void Run(string[] args)
         {
-            var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
-            Console.WriteLine(MyFunction(input));
+            var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).First();
+            Console.WriteLine(MinimumNumberOfCoinsToChangeMoney(input));
         }
 
-        public string MyFunction(long[] n)
+        public int MinimumNumberOfCoinsToChangeMoney(int n)
         {
-            return "hello";
+            var countOf10s = 0;
+            var countOf5sAnd1s = 0;
+
+            if (n < 5)
+            {
+                return n;
+            }
+            else if (n >= 5 && n <= 10)
+            {
+                return n % 5 + 1;
+            }
+            else if (n > 10)
+            {
+                countOf10s = n / 10;
+                countOf5sAnd1s = MinimumNumberOfCoinsToChangeMoney(n % 10);
+            }
+
+            var coinCount = countOf10s + countOf5sAnd1s;
+
+            return coinCount;
         }
     }
 }
