815f2c5b6ab4892c917c65b89564e2e1d0e6ffef
=brntsllvn <=brntsllvn@gmail.com>
2016-06-17 09:01:33 -0700

    partial set up for knapsack problem

---
+++ FractionalKnapsack/FractionalKnapsackTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Diagnostics;
+
+namespace FractionalKnapsack
+{
+    [TestFixture]
+    class FractionalKnapsackTests
+    {
+
+        //private Launcher.ValueAndWeight[1] _test1ValuesAndWeights;
+
+
+        //        new Launcher.ValueAndWeight { Value = 15, Weight = 12 }
+
+        public Launcher.ValueAndWeight[] Test1Array
+        {
+            get
+            {
+                var arr = new Launcher.ValueAndWeight[1];
+                arr[0] = new Launcher.ValueAndWeight { Value = 15, Weight = 10 };
+                return arr;
+            }
+        }
+
+
+        [TestCase(0.0, FractionalKnapsackTests.Test1Array)]
+        public void Test_1(decimal expected, Launcher.ValueAndWeight[] input)
+        {
+            var f0 = new Launcher();
+            f0.FractionalKnapsackCalculator(input).ShouldBe(expected);
+
+            Should.CompleteIn(
+                () => f0.FractionalKnapsackCalculator(input), TimeSpan.FromMilliseconds(1500));
+
+            var proc = Process.GetCurrentProcess();
+            proc.PrivateMemorySize64.ShouldBeLessThanOrEqualTo(512 * 1000 * 1000);
+        }
+    }
+}
---
+++ FractionalKnapsack/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace FractionalKnapsack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Launcher().Run(args);
+        }
+    }
+
+    public class Launcher
+    {
+        public void Run(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
+            var numItems = input[0];
+            var knapsackCapacity = input[1];
+
+            var valueAndWeightInput = new ValueAndWeight[numItems];
+
+            for (int i = 0; i < numItems; i++)
+            {
+                input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
+
+                valueAndWeightInput[i] = new ValueAndWeight
+                {
+                    Value = input[0],
+                    Weight = input[1]
+                };
+            }
+
+
+
+            Console.WriteLine(FractionalKnapsackCalculator(valueAndWeightInput));
+        }
+
+        public decimal FractionalKnapsackCalculator(ValueAndWeight[] valueWeightArray)
+        {
+            return 0;
+        }
+
+        public class ValueAndWeight
+        {
+            public long Value { get; set; }
+            public long Weight { get; set; }
+        }
+    }
+}
