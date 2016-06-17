cb4483ac332e9df07ca86a24d43a6fc46f1dfeb6
=brntsllvn <=brntsllvn@gmail.com>
2016-06-17 07:40:10 -0700

    week 3 add project solution file: changing money

---
+++ ChangingMoney/ChangingMoneyTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Diagnostics;
+
+namespace ChangingMoney
+{
+    [TestFixture]
+    class CMTests
+    {
+        [TestCase(new long[] { 0 }, "hello")]
+        public void Test_1(long[] input, string expected)
+        {
+            var f0 = new Launcher();
+            f0.MyFunction(input).ShouldBe(expected);
+
+            Should.CompleteIn(
+                () => f0.MyFunction(input), TimeSpan.FromMilliseconds(1500));
+
+            var proc = Process.GetCurrentProcess();
+            proc.PrivateMemorySize64.ShouldBeLessThanOrEqualTo(512 * 1000 * 1000);
+        }
+    }
+}
--- Week1_MaximumPairwiseProduct/MaximumPairwiseProductTests.cs
+++ Week1_MaximumPairwiseProduct/MaximumPairwiseProductTests.cs
@@ -4,21 +4,21 @@ using Shouldly;
 
 namespace Week1_MaximumPairwiseProduct
 {
-    [TestFixture]
-    class MaximumPairwiseProductTests
-    {
-        [TestCase(new long[] { 1, 2, 3 }, 6, 1)]
-        [TestCase(new long[] { 7, 5, 14, 2, 8, 8, 10, 1, 2, 3 }, 140, 1)]
-        [TestCase(new long[] { 4, 6, 2, 6, 1 }, 36, 1)]
-        [TestCase(new long[] { 100000, 90000 }, 9000000000, 1)]
-        public void Pairwise_Product1(long[] longArr, long expectedProduct, int completionTime)
-        {
-            var product = new Launcher().FindMaxPairwiseProduct(longArr);
+    //[TestFixture]
+    //class MaximumPairwiseProductTests
+    //{
+    //    [TestCase(new long[] { 1, 2, 3 }, 6, 1)]
+    //    [TestCase(new long[] { 7, 5, 14, 2, 8, 8, 10, 1, 2, 3 }, 140, 1)]
+    //    [TestCase(new long[] { 4, 6, 2, 6, 1 }, 36, 1)]
+    //    [TestCase(new long[] { 100000, 90000 }, 9000000000, 1)]
+    //    public void Pairwise_Product1(long[] longArr, long expectedProduct, int completionTime)
+    //    {
+    //        var product = new Launcher().FindMaxPairwiseProduct(longArr);
 
-            product.ShouldBe(expectedProduct);
-            Should.CompleteIn(() => product, TimeSpan.FromMilliseconds(completionTime));
-            long memory = GC.GetTotalMemory(true);
-            memory.ShouldBeLessThanOrEqualTo(1000000);
-        }
-    }
+    //        product.ShouldBe(expectedProduct);
+    //        Should.CompleteIn(() => product, TimeSpan.FromMilliseconds(completionTime));
+    //        long memory = GC.GetTotalMemory(true);
+    //        memory.ShouldBeLessThanOrEqualTo(1000000);
+    //    }
+    //}
 }
