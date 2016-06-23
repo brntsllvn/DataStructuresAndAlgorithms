fa59e1a95d0ed4c447cc8cc3a3adfd1dc2619eb5
Brent Sullivan <brntsllvn@gmail.com>
2016-06-23 08:57:32 -0700

    min dot prod test cases

--- MinDotProd/MinDotProdTests.cs
+++ MinDotProd/MinDotProdTests.cs
@@ -8,9 +8,12 @@ namespace MinDotProd
     [TestFixture]
     class MinDotProdTests
     {
-        [TestCase(new int[1] { 23 }, new int[1] { 39 }, 897)]
-        [TestCase(new int[3] { 1, 3, -5 }, new int[3] { -2, 4, 1 }, -25)]
-        public void Test_1(int[] first, int[] second, int expected)
+        [TestCase(new long[1] { 23 }, new long[1] { 39 }, 897)]
+        [TestCase(new long[1] { 99999 }, new long[1] { 99999 }, 9999800001)]
+        [TestCase(new long[2] { 5, 5 }, new long[2] { 0, 3 }, 15)]
+        [TestCase(new long[3] { 1, 3, -5 }, new long[3] { -2, 4, 1 }, -25)]
+        [TestCase(new long[2] { -10000, 10000 }, new long[2] { -1, 1 }, -20000)]
+        public void Test_1(long[] first, long[] second, long expected)
         {
             var f0 = new Launcher();
             f0.ComputeMinDotProduct(first, second).ShouldBe(expected);
--- MinDotProd/Program.cs
+++ MinDotProd/Program.cs
@@ -16,25 +16,25 @@ namespace MinDotProd
         public void Run(string[] args)
         {
             var numInts = Console.ReadLine();
-            var firstSequence = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
-            var secondSequence = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+            var firstSequence = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
+            var secondSequence = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
             Console.WriteLine(ComputeMinDotProduct(firstSequence, secondSequence));
         }
 
-        public int ComputeMinDotProduct(int[] first, int[] second)
+        public long ComputeMinDotProduct(long[] first, long[] second)
         {
-            if (first.Length == 1)
-                return first[0] * second[0];
-
             var descendingFirst = first.OrderByDescending(x => x).ToArray();
             var ascendingSecond = second.OrderBy(x => x).ToArray();
 
-            var descendingFirstLessOne = new int[descendingFirst.Length - 1];
-            for (int i = 0; i < descendingFirst.Length - 1; i++)
+            if (descendingFirst.Length == 1)
+                return descendingFirst[0] * ascendingSecond[0];
+
+            var descendingFirstLessOne = new long[descendingFirst.Length - 1];
+            for (long i = 0; i < descendingFirst.Length - 1; i++)
                 descendingFirstLessOne[i] = descendingFirst[i+1];
 
-            var ascendingSecondLessOne = new int[ascendingSecond.Length - 1];
-            for (int i = 0; i < ascendingSecond.Length - 1; i++)
+            var ascendingSecondLessOne = new long[ascendingSecond.Length - 1];
+            for (long i = 0; i < ascendingSecond.Length - 1; i++)
                 ascendingSecondLessOne[i] = ascendingSecond[i+1];
 
             var maxMinProduct = descendingFirst[0] * ascendingSecond[0];
