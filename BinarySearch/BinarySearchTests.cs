171f5ced60a75b4206aa25504b210f3034db8781
Brent Sullivan <brntsllvn@gmail.com>
2016-06-23 08:42:01 -0700

    NUnit test runner acting strangely...

--- BinarySearch/BinarySearchTests.cs
+++ BinarySearch/BinarySearchTests.cs
@@ -8,14 +8,22 @@ namespace BinarySearch
     [TestFixture]
     class BinarySearchTests
     {
-        [TestCase(new long[] { 0 }, "hello")]
-        public void Test_1(long[] input, string expected)
+        [TestCase(new long[] { 1, 13 }, new long[] { 1, 13 }, "0")]
+        [TestCase(new long[] { 1, 13 }, new long[] { 2, 13, 13 }, "0 0")]
+        [TestCase(new long[] { 1, 13 }, new long[] { 3, 13, 13, 13 }, "0 0 0")]
+        [TestCase(new long[] { 1, 13 }, new long[] { 1, 7 }, "-1")]
+        [TestCase(new long[] { 1, 13 }, new long[] { 2, 7, 7 }, "-1 -1")]
+        [TestCase(new long[] { 1, 13 }, new long[] { 3, 7, 7, 7 }, "-1 -1 -1")]
+        [TestCase(new long[] { 1, 13 }, new long[] { 3, 7, 13, 7 }, "-1 0 -1")]
+        //[TestCase(new long[] { 2, 13, 87 }, new long[] { 1, 87 }, "0")]
+        [TestCase(new long[] { 5, 1, 5, 8, 12, 13 }, new long[] { 5, 8, 1, 23, 1, 11 }, "2 0 -1 0 -1")]
+        public void BinarySearch_GameOn(long[] data, long[] searchTerms, string expected)
         {
             var f0 = new Launcher();
-            f0.MyFunction(input).ShouldBe(expected);
+            f0.BinarySearch(data, searchTerms).ShouldBe(expected);
 
             Should.CompleteIn(
-                () => f0.MyFunction(input), TimeSpan.FromMilliseconds(1500));
+            () => f0.BinarySearch(data, searchTerms), TimeSpan.FromMilliseconds(3000));
 
             var proc = Process.GetCurrentProcess();
             proc.PrivateMemorySize64.ShouldBeLessThanOrEqualTo(512 * 1000 * 1000);
--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -16,12 +16,43 @@ namespace BinarySearch
         public void Run(string[] args)
         {
             var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
-            Console.WriteLine(MyFunction(input));
+            var search = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
+            Console.WriteLine(BinarySearch(input, search));
         }
 
-        public string MyFunction(long[] n)
+        public string BinarySearch(long[] data, long[] searchTerms)
         {
-            return "hello";
+            var numSearchableDataElements = data[0];
+            var searchableData = new long[numSearchableDataElements];
+            for (int i = 0; i < numSearchableDataElements; i++)
+            {
+                searchableData[i] = data[i + 1];
+            }
+
+            var numSearchTerms = searchTerms[0];
+            var actualSearchTerms = new long[numSearchTerms];
+            for (int i = 0; i < numSearchTerms; i++)
+            {
+                actualSearchTerms[i] = searchTerms[i + 1];
+            }
+
+            var resultString = "";
+            if (numSearchableDataElements == 1)
+            {
+                for (int i = 0; i < numSearchTerms; i++)
+                {
+                    if (actualSearchTerms[i] == searchableData[0])
+                    {
+                        resultString += "0 ";
+                    }
+                    else
+                    {
+                        resultString += "-1 ";
+                    }
+                }
+            }
+
+            return resultString.Trim();
         }
     }
 }
