5ad862883444bda640352e7f6814c9d9908d6ba5
Brent Sullivan <brntsllvn@gmail.com>
2016-06-24 16:01:45 -0700

    Majority search first stab

---
+++ MajorityElement/MajorityElementTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using Shouldly;
+
+namespace MajorityElement
+{
+    class MajorityElementTests
+    {
+        [TestCase(new long[] { 2 }, 1)]
+        [TestCase(new long[] { 2, 1 }, 0)]
+        [TestCase(new long[] { 2, 2 }, 1)]
+        [TestCase(new long[] { 2, 2, 2 }, 1)]
+        //[TestCase(new long[] { 2, 2, 2, 3, 9, }, 1)]
+        //[TestCase(new long[] { 1, 2, 3, 4, 5, 6 }, 0)]
+        //[TestCase(new long[] { 1, 1, 2, 3 }, 0)]
+        //[TestCase(new long[] { 1, 2, 2, 2, 3 }, 1)]
+        //[TestCase(new long[] { 1, 1, 2, 2, 2 }, 1)]
+        public void Test_1(long[] input, int expected)
+        {
+            var f0 = new Launcher();
+            f0.HasMajorityElement(input, input.Length).ShouldBe(expected);
+        }
+    }
+}
---
+++ MajorityElement/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace MajorityElement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Launcher().Run(args);
+        }
+    }
+
+    public class Launcher
+    {
+        public void Run(string[] args)
+        {
+            var numInputs = int.Parse(Console.ReadLine());
+            var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
+            var sortedInput = input.OrderBy(x => x).ToArray();
+            Console.WriteLine(HasMajorityElement(sortedInput, numInputs));
+        }
+
+        public int HasMajorityElement(long[] n, int numElementsUnderConsideration)
+        {
+            if (numElementsUnderConsideration == 1)
+                return 1;
+
+            var midPoint = numElementsUnderConsideration / 2;
+
+            if (numElementsUnderConsideration == 2)
+            {
+                if (n[midPoint - 1] == n[midPoint])
+                {
+                    return 1;
+                }
+                return 0;
+            }
+
+            if (n[midPoint] != n[midPoint + 1])
+                return 0;
+            return HasMajorityElement(n, numElementsUnderConsideration / 2);
+        }
+    }
+}
