c26998dd11aa947239300de5f8f51b53ce35828e
Brent Sullivan <brntsllvn@gmail.com>
2016-06-25 10:53:35 -0700

    fix test runner problem

--- MajorityElement/MajorityElementTests.cs
+++ MajorityElement/MajorityElementTests.cs
@@ -5,18 +5,20 @@ using Shouldly;
 
 namespace MajorityElement
 {
+    [TestFixture]
     class MajorityElementTests
     {
-        [TestCase(new long[] { 2 }, 1)]
-        [TestCase(new long[] { 2, 1 }, 0)]
-        [TestCase(new long[] { 2, 2 }, 1)]
-        [TestCase(new long[] { 2, 2, 2 }, 1)]
-        //[TestCase(new long[] { 2, 2, 2, 3, 9, }, 1)]
-        //[TestCase(new long[] { 1, 2, 3, 4, 5, 6 }, 0)]
-        //[TestCase(new long[] { 1, 1, 2, 3 }, 0)]
-        //[TestCase(new long[] { 1, 2, 2, 2, 3 }, 1)]
-        //[TestCase(new long[] { 1, 1, 2, 2, 2 }, 1)]
-        public void Test_1(long[] input, int expected)
+        [TestCase("A", new long[] { 2 }, 1)]
+        [TestCase("B", new long[] { 2, 1 }, 0)]
+        [TestCase("C", new long[] { 2, 2 }, 1)]
+        [TestCase("D", new long[] { 2, 2, 2 }, 1)]
+        [TestCase("E", new long[] { 2, 2, 2, 3, 9, }, 1)]
+        //[TestCase("F", new long[] { 1, 2, 3, 4, 5, 6 }, 0)]
+        //[TestCase("G", new long[] { 1, 1, 2, 3 }, 0)]
+        //[TestCase("H", new long[] { 1, 2, 2, 2, 3 }, 1)]
+        //[TestCase("I", new long[] { 1, 1, 2, 2, 2 }, 1)]
+        //[TestCase("J", new long[] { 1, 1, 2, 2, 2, 2, 2, 2, 3, 3 }, 0)]
+        public void Test_1(string testCase, long[] input, int expected)
         {
             var f0 = new Launcher();
             f0.HasMajorityElement(input, input.Length).ShouldBe(expected);
