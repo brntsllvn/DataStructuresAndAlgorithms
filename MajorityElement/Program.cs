43841e8331815af0114320bce164d4d2fbab4c87
Brent Sullivan <brntsllvn@gmail.com>
2016-06-28 12:37:27 -0700

    Another solution for Majority Element

--- MajorityElement/Program.cs
+++ MajorityElement/Program.cs
@@ -17,44 +17,43 @@ namespace MajorityElement
         {
             var numInputs = long.Parse(Console.ReadLine());
             var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
-            Console.WriteLine(Majority(input, 0, input.Length - 1));
+            Console.WriteLine(Majority(input, 0, input.Length - 1) > 0 ? 1 : 0);
         }
 
         public long Majority(long[] n, long low, long high)
         {
+            if (n.Length == 1)
+                return n[low];
+
             if (high - low == 0)
-                if (IsMajorityElement(n, n[low]))
-                    return 1;
-                
-            if (high - low == 1)
-                if(n[low] == n[high] && IsMajorityElement(n, n[low]))
-                    return 1;
+                return n[low];
 
-            var mid = low + (high - low) / 2;
+            var mid = low + (high - low)/2;
 
-            if (high - low > 1)
-            {
-                var lResult = Majority(n, low, mid);
-                if (lResult == 1)
-                    return 1;
-                var rResult = Majority(n, mid + 1, high);
-                if (rResult == 1)
-                    return 1;
-            }
+            long mL = Majority(n, low, mid);
+            long mR = Majority(n, mid + 1, high);
 
+            int hurdle;
+            if ((high - low)%2 == 0)
+                hurdle = (int) ((high - low)/2);
+            else
+                hurdle = (int) ((high - low)/2 + 1);
+
+            int count = 0;
+            for (int i = (int) low; i < high+1; i++)
+                if (n[i] == mL)
+                    count++;
+            if (count > hurdle)
+                return mL;
+
+            count = 0;
+            for (int i = (int) low; i < high+1; i++)
+                if (n[i] == mR)
+                    count++;
+            if (count > hurdle)
+                return mR;
+        
             return 0;
-        }
-
-        private bool IsMajorityElement(long[] n, long candidate)
-        {
-            var candidateInstanceCounter = 0;
-            for (int i = 0; i < n.Length; i++)
-                if (n[i] == candidate)
-                    candidateInstanceCounter++;
-
-            var hurdle = n.Length / 2;
-
-            return candidateInstanceCounter > hurdle;
         }
     }
 }
