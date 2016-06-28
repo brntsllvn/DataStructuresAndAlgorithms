3073163490257ed9a12a6a19d039c08bbbf12617
Brent Sullivan <brntsllvn@gmail.com>
2016-06-27 17:23:19 -0700

    more work on majority element

--- MajorityElement/MajorityElementTests.cs
+++ MajorityElement/MajorityElementTests.cs
@@ -14,26 +14,25 @@ namespace MajorityElement
         [TestCase("D0", new long[] { 2, 2, 2 }, 1)]
         [TestCase("D1", new long[] { 2, 2, 3 }, 1)]
         [TestCase("D2", new long[] { 2, 3, 4 }, 0)]
-        [TestCase("E0", new long[] { 1, 2, 3, 4 }, 0)]
-        [TestCase("E1", new long[] { 1, 1, 3, 4 }, 0)]
-        [TestCase("E2", new long[] { 0, 1, 1, 4 }, 0)]
-        [TestCase("E3", new long[] { 0, 1, 2, 2 }, 0)]
-        [TestCase("E4", new long[] { 0, 2, 2, 2 }, 1)]
-        [TestCase("E5", new long[] { 2, 2, 2, 0 }, 1)]
-        [TestCase("E6", new long[] { 2, 2, 2, 2 }, 1)]
-        [TestCase("F", new long[] { 2, 2, 2, 3, 9, }, 1)]
-        [TestCase("G", new long[] { 1, 2, 3, 4, 5, 6 }, 0)]
-        [TestCase("H", new long[] { 1, 2, 2, 2, 3 }, 1)]
-        [TestCase("I", new long[] { 1, 1, 2, 2, 2 }, 1)]
-        [TestCase("J", new long[] { 1, 1, 2, 2, 2, 2, 2, 2, 3, 3 }, 1)]
-        [TestCase("K", new long[] { 1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 3 }, 0)]
-        [TestCase("L", new long[] { 1, 1, 1, 1, 2, 2, 3, 3, 3, 3 }, 0)]
-        //[TestCase("M", new long[] { 1, 1, 1, 2, 2 }, 1)]
+        //[TestCase("E0", new long[] { 1, 2, 3, 4 }, 0)]
+        //[TestCase("E1", new long[] { 1, 1, 3, 4 }, 0)]
+        //[TestCase("E2", new long[] { 0, 1, 1, 4 }, 0)]
+        //[TestCase("E3", new long[] { 0, 1, 2, 2 }, 0)]
+        //[TestCase("E4", new long[] { 0, 2, 2, 2 }, 1)]
+        //[TestCase("E5", new long[] { 2, 2, 2, 0 }, 1)]
+        //[TestCase("E6", new long[] { 2, 2, 2, 2 }, 1)]
+        //[TestCase("F", new long[] { 2, 2, 2, 3, 9, }, 1)]
+        //[TestCase("G", new long[] { 1, 2, 3, 4, 5, 6 }, 0)]
+        //[TestCase("H", new long[] { 1, 2, 2, 2, 3 }, 1)]
+        //[TestCase("I", new long[] { 1, 1, 2, 2, 2 }, 1)]
+        //[TestCase("J", new long[] { 1, 1, 2, 2, 2, 2, 2, 2, 3, 3 }, 1)]
+        //[TestCase("K", new long[] { 1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 3 }, 0)]
+        //[TestCase("L", new long[] { 1, 1, 1, 1, 2, 2, 3, 3, 3, 3 }, 0)]
+        ////[TestCase("M", new long[] { 1, 1, 1, 2, 2 }, 1)]
         public void Test_1(string testCase, long[] input, int expected)
         {
             var f0 = new Launcher();
-            var majorityCandidate = input[(input.Length - 1) / 2];
-            f0.HasMajorityElement(input, 0, input.Length - 1, majorityCandidate).ShouldBe(expected);
+            f0.Majority(input, 0, input.Length - 1).ShouldBe(expected);
         }
     }
 }
--- MajorityElement/Program.cs
+++ MajorityElement/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.CodeDom;
 using System.Linq;
+using NUnit.Core;
+using Shouldly.Configuration;
 
 namespace MajorityElement
 {
@@ -15,33 +18,50 @@ namespace MajorityElement
     {
         public void Run(string[] args)
         {
-            var numInputs = int.Parse(Console.ReadLine());
+            var numInputs = long.Parse(Console.ReadLine());
             var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
-            var sortedInput = input.OrderBy(x => x).ToArray();
-            var majorityCandidate = sortedInput[(sortedInput.Length - 1) / 2];
-            Console.WriteLine(HasMajorityElement(sortedInput, 0, sortedInput.Length - 1, majorityCandidate));
+            Console.WriteLine(Majority(input, 0, input.Length - 1));
         }
 
-        public int HasMajorityElement(long[] n, int low, int high, long candidate)
+        public long Majority(long[] n, long low, long high)
         {
-            var sum = 0;
-            for (int i = 0; i < n.Length; i++)
-            {
-                if (n[i] == candidate)
-                    sum++;
-            }
-
-            var hurdle = 0;
-
-            if (n.Length % 2 == 0)
-                hurdle = high / 2 + 1;
-            else
-                hurdle = high / 2;
-
-            if (sum > hurdle)
+            if (high - low == 0)
                 return 1;
 
+            if (high - low == 1)
+            {
+                return n[low] == n[high] ? 1 : 0;
+            }
+
+            var mid = low + (high - low) / 2;
+
+            if (high - low > 0)
+            {
+                var lResult = Majority(n, low, mid);
+                var rResult = Majority(n, mid + 1, high);
+
+                if (IsMajorityElement(n, low, high, lResult))
+                    return 1;
+
+                if (IsMajorityElement(n, low, high, rResult))
+                    return 1;
+            }
+
             return 0;
+        }
+
+        private bool IsMajorityElement(long[] n, long low, long high, long candidate)
+        {
+            var subN = new long[high - low + 1];
+            for (int i = 0, j = (int)low; i < subN.Length; i++, j++)
+                subN[i] = n[j];
+
+            var hurdle = (high - low) / 2;
+
+            var candidateCount = subN.Select(x => x == candidate).Count();
+
+            return candidateCount > hurdle;
+
         }
     }
 }
