e4c575da5f9af4f3fc2c8f2836ad995b07a0e920
Brent Sullivan <brntsllvn@gmail.com>
2016-06-29 11:22:48 -0700

    Add Merge Sort solution. Convert all projects to x64.

---
+++ MergeSort/MergeSortTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace MergeSort
+{
+    [TestFixture]
+    class MergeSortTests
+    {
+        [TestCase("A", new long[] { 0 }, new long[] { 0 })]
+        public void Test_1(string name, long[] input, long[] expected)
+        {
+            var f0 = new Launcher();
+            f0.MergeSort(input).ShouldBe(expected);
+
+            //Should.CompleteIn(
+            //    () => f0.MergeSort(input), TimeSpan.FromMilliseconds(1500));
+
+            //var proc = Process.GetCurrentProcess();
+            //proc.PrivateMemorySize64.ShouldBeLessThanOrEqualTo(512 * 1000 * 1000);
+        }
+    }
+}
---
+++ MergeSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace MergeSort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Launcher().Run(args);
+        }
+    }
+
+    public class Launcher
+    {
+        public void Run(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
+            Console.WriteLine(MergeSort(input));
+        }
+
+        public long[] MergeSort(long[] n)
+        {
+            return new long[1] {0};
+        }
+    }
+}
