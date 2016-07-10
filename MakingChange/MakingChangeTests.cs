b6c962f07e7a8361a0bf1b9e4a474c3a6bb1cde9
Brent Sullivan <brntsllvn@gmail.com>
2016-07-10 11:42:55 -0700

    Dynamic Programming: Making Change Solution

---
+++ MakingChange/MakingChangeTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace MakingChange
+{
+
+    [TestFixture]
+    class MakingChangeTests
+    {
+        [TestCase("A USD", new int[] { 1, 5, 10, 25, 50 }, 0, 0)]
+        [TestCase("B USD", new int[] { 1, 5, 10, 25, 50 }, 1, 1)]
+        [TestCase("C USD", new int[] { 1, 5, 10, 25, 50 }, 2, 2)]
+        [TestCase("D USD", new int[] { 1, 5, 10, 25, 50 }, 5, 1)]
+        public void MakeChange(string caseName, int[] coinDenominations, int amountToChange, int expectedNumCoins)
+        {
+            var f0 = new Launcher();
+            f0.DynamicProgrammingMakeChange(coinDenominations, amountToChange).ShouldBe(expectedNumCoins);
+        }
+    }
+}
---
+++ MakingChange/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace MakingChange
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Launcher().Run(args);
+        }
+    }
+
+    public class Launcher
+    {
+        public void Run(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
+        }
+
+        public int DynamicProgrammingMakeChange(int[] coinDenominations, int amountToChange)
+        {
+            if (amountToChange == 0)
+                return 0;
+
+            var minNumberCoins = new int[amountToChange + 1];
+
+            for (int m = 1; m <= amountToChange; m++)
+            {
+                minNumberCoins[m] = int.MaxValue;
+
+                for (int i = 0; i < coinDenominations.Length; i++)
+                {
+                    if (m >= coinDenominations[i])
+                    {
+                        var NumCoins = minNumberCoins[m-coinDenominations[i]] + 1;
+
+                        if (NumCoins < minNumberCoins[m])
+                        {
+                            minNumberCoins[m] = NumCoins;
+                        }
+                    }
+                }
+            }
+
+            return minNumberCoins[amountToChange];
+        }
+    }
+}
