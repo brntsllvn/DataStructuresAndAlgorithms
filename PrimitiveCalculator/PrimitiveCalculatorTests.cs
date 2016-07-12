027a6533c85d9950fc15cec8898345d9a5e7631a
Brent Sullivan <brntsllvn@gmail.com>
2016-07-12 08:21:01 -0700

    Primitive calculator: all tests passing. Need to add prescribed output generator.

--- PrimitiveCalculator/PrimitiveCalculatorTests.cs
+++ PrimitiveCalculator/PrimitiveCalculatorTests.cs
@@ -1,8 +1,5 @@
 ﻿using NUnit.Framework;
 using Shouldly;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 
 namespace PrimitiveCalculator
 {
@@ -14,14 +11,39 @@ namespace PrimitiveCalculator
         [TestCase(3, 1)]
         [TestCase(4, 2)]
         [TestCase(5, 3)]
+        [TestCase(6, 2)]
+        [TestCase(7, 3)]
         [TestCase(8, 3)]
+        [TestCase(9, 2)]
+        [TestCase(10, 3)]
+        [TestCase(11, 4)]
         [TestCase(12, 3)]
         [TestCase(16, 4)]
         [TestCase(96234, 14)]
-        public void Test_1(int n, int expectedNumOpertations)
+        public void NumOpsTests(int n, int expectedNumOpertations)
         {
             var f0 = new Launcher();
             f0.PrimitiveCalculatorOperationCounter(n).ShouldBe(expectedNumOpertations);
+        }
+
+        [TestCase(0, 2, false)]
+        [TestCase(1, 2, true)]
+        [TestCase(2, 2, true)]
+        [TestCase(3, 2, false)]
+        [TestCase(7, 2, false)]
+        [TestCase(16, 2, true)]
+        [TestCase(57, 2, false)]
+        [TestCase(1024, 2, true)]
+        [TestCase(1, 3, true)]
+        [TestCase(3, 3, true)]
+        [TestCase(4, 3, false)]
+        [TestCase(27, 3, true)]
+        [TestCase(28, 3, false)]
+        [TestCase(59049, 3, true)]
+        public void IsPow(int n, int x, bool expected)
+        {
+            var f0 = new Launcher();
+            f0.IsPowerOfX(n, x).ShouldBe(expected);
         }
     }
 }
--- PrimitiveCalculator/Program.cs
+++ PrimitiveCalculator/Program.cs
@@ -24,27 +24,50 @@ namespace PrimitiveCalculator
             if (n == 1)
                 return 0;
 
-            var minNumOperations = new int[n+1];
+            var minNumOperations = new int[n + 1];
             minNumOperations[0] = 0;
             minNumOperations[1] = 0;
 
             for (int i = 2; i <= n; i++)
             {
-                if (i % 2 != 0 && i % 3 != 0)
+                if (IsPowerOfX(i,3))
                 {
-                    minNumOperations[i] = minNumOperations[i / 2] + 2;
+                    minNumOperations[i] = minNumOperations[i / 3] + 1;
                 }
-                else if (i % 2 == 0)
+                else if (IsPowerOfX(i,2))
                 {
                     minNumOperations[i] = minNumOperations[i / 2] + 1;
                 }
                 else if (i % 3 == 0)
                 {
                     minNumOperations[i] = minNumOperations[i / 3] + 1;
-                }                    
+                }
+                else if (i % 3 == 1)
+                {
+                    minNumOperations[i] = minNumOperations[i / 3] + 2;
+                }
+                else if (i % 2 == 0)
+                {
+                    minNumOperations[i] = minNumOperations[i / 2] + 1;
+                }
+                else if (i % 3 == 2)
+                {
+                    minNumOperations[i] = minNumOperations[i / 3] + 3;
+                }
             }
 
             return minNumOperations[n];
+        }
+
+        public bool IsPowerOfX(int n, int x)
+        {
+            if (n == 0)
+                return false;
+
+            for (int i = 1; i < Math.Log(n,x); i++)
+                if (n % Math.Pow(x,i) != 0)
+                    return false;
+            return true;
         }
     }
 }
