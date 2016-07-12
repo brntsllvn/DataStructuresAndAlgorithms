54263583c5e0ee2861572431c1157e6f130683f1
Brent Sullivan <brntsllvn@gmail.com>
2016-07-11 18:32:12 -0700

    Primitive Calculator partial solution

--- PrimitiveCalculator/PrimitiveCalculatorTests.cs
+++ PrimitiveCalculator/PrimitiveCalculatorTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using Shouldly;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace PrimitiveCalculator
@@ -8,16 +9,19 @@ namespace PrimitiveCalculator
     [TestFixture]
     class PrimitiveCalculatorTests
     {
-        [TestCase(1, 0, new int[] { 1 })]
-        public void Test_1(int n, int expectedNumOpertations, int[] expectedIntermediateInts)
+        [TestCase(1, 0)]
+        [TestCase(2, 1)]
+        [TestCase(3, 1)]
+        [TestCase(4, 2)]
+        [TestCase(5, 3)]
+        [TestCase(8, 3)]
+        [TestCase(12, 3)]
+        [TestCase(16, 4)]
+        [TestCase(96234, 14)]
+        public void Test_1(int n, int expectedNumOpertations)
         {
             var f0 = new Launcher();
-            f0.MyFunction(n).ShouldBe(expectedNumOpertations);
-
-            Should.CompleteIn(() => f0.MyFunction(n), TimeSpan.FromMilliseconds(1500));
-
-            var proc = Process.GetCurrentProcess();
-            proc.PrivateMemorySize64.ShouldBeLessThanOrEqualTo(512 * 1000 * 1000);
+            f0.PrimitiveCalculatorOperationCounter(n).ShouldBe(expectedNumOpertations);
         }
     }
 }
--- PrimitiveCalculator/Program.cs
+++ PrimitiveCalculator/Program.cs
@@ -16,12 +16,35 @@ namespace PrimitiveCalculator
         public void Run(string[] args)
         {
             var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
-            Console.WriteLine(MyFunction(input[0]));
+            Console.WriteLine(PrimitiveCalculatorOperationCounter(input[0]));
         }
 
-        public int MyFunction(int n)
+        public int PrimitiveCalculatorOperationCounter(int n)
         {
-            return -1;
+            if (n == 1)
+                return 0;
+
+            var minNumOperations = new int[n+1];
+            minNumOperations[0] = 0;
+            minNumOperations[1] = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                if (i % 2 != 0 && i % 3 != 0)
+                {
+                    minNumOperations[i] = minNumOperations[i / 2] + 2;
+                }
+                else if (i % 2 == 0)
+                {
+                    minNumOperations[i] = minNumOperations[i / 2] + 1;
+                }
+                else if (i % 3 == 0)
+                {
+                    minNumOperations[i] = minNumOperations[i / 3] + 1;
+                }                    
+            }
+
+            return minNumOperations[n];
         }
     }
 }
