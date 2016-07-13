3fb34bd5684de0380e2a2fd2b484def4593f80ba
Brent Sullivan <brntsllvn@gmail.com>
2016-07-13 14:53:05 -0700

    steps toward final solution

--- PrimitiveCalculator/PrimitiveCalculatorTests.cs
+++ PrimitiveCalculator/PrimitiveCalculatorTests.cs
@@ -6,23 +6,24 @@ namespace PrimitiveCalculator
     [TestFixture]
     class PrimitiveCalculatorTests
     {
-        [TestCase(1, 0)]
-        [TestCase(2, 1)]
-        [TestCase(3, 1)]
-        [TestCase(4, 2)]
-        [TestCase(5, 3)]
-        [TestCase(6, 2)]
-        [TestCase(7, 3)]
-        [TestCase(8, 3)]
-        [TestCase(9, 2)]
-        [TestCase(10, 3)]
-        [TestCase(11, 4)]
-        [TestCase(12, 3)]
-        [TestCase(16, 4)]
-        [TestCase(96234, 14)]
-        public void NumOpsTests(int n, int expectedNumOpertations)
+        [TestCase(1, 0, new int[0])]
+        //[TestCase(2, 2, new int[2] { 1, 2 })]
+        [TestCase(3, 2, new int[2] { 1, 3 } )]
+        //[TestCase(4, 2)]
+        //[TestCase(5, 3)]
+        //[TestCase(6, 2)]
+        //[TestCase(7, 3)]
+        //[TestCase(8, 3)]
+        //[TestCase(9, 2)]
+        //[TestCase(10, 3)]
+        //[TestCase(11, 4)]
+        //[TestCase(12, 3)]
+        //[TestCase(16, 4)]
+        //[TestCase(96234, 14)]
+        public void NumOpsTests(int n, int expectedLength, int[] expectedNumOpertations)
         {
             var f0 = new Launcher();
+            //f0.PrimitiveCalculatorOperationCounter(n).Length.ShouldBe(expectedLength);
             f0.PrimitiveCalculatorOperationCounter(n).ShouldBe(expectedNumOpertations);
         }
 
--- PrimitiveCalculator/Program.cs
+++ PrimitiveCalculator/Program.cs
@@ -19,10 +19,14 @@ namespace PrimitiveCalculator
             Console.WriteLine(PrimitiveCalculatorOperationCounter(input[0]));
         }
 
-        public int PrimitiveCalculatorOperationCounter(int n)
+        public int[] PrimitiveCalculatorOperationCounter(int n)
         {
             if (n == 1)
-                return 0;
+                return new int[0];
+
+            var integerSequence = new int[n + 1];
+            integerSequence[0] = 0;
+            integerSequence[1] = 1;
 
             var minNumOperations = new int[n + 1];
             minNumOperations[0] = 0;
@@ -33,30 +37,36 @@ namespace PrimitiveCalculator
                 if (IsPowerOfX(i,3))
                 {
                     minNumOperations[i] = minNumOperations[i / 3] + 1;
+                    //integerSequence[i] = integerSequence[i / 3], 3x;
                 }
                 else if (IsPowerOfX(i,2))
                 {
                     minNumOperations[i] = minNumOperations[i / 2] + 1;
+                    //integerSequence[i] = integerSequence[i / 3], 2x;
                 }
                 else if (i % 3 == 0)
                 {
                     minNumOperations[i] = minNumOperations[i / 3] + 1;
+                    //integerSequence[i] = integerSequence[i / 3], 3x;
                 }
                 else if (i % 3 == 1)
                 {
                     minNumOperations[i] = minNumOperations[i / 3] + 2;
+                    //integerSequence[i] = integerSequence[i / 3], 3x, x+1;
                 }
                 else if (i % 2 == 0)
                 {
                     minNumOperations[i] = minNumOperations[i / 2] + 1;
+                    //integerSequence[i] = integerSequence[i / 3], 2x;
                 }
                 else if (i % 3 == 2)
                 {
                     minNumOperations[i] = minNumOperations[i / 3] + 3;
+                    //integerSequence[i] = integerSequence[i / 3], 3x, x+1, x+1;
                 }
             }
 
-            return minNumOperations[n];
+            return integerSequence.Select(x => x != 0).ToArray; // remove all trailing zeros
         }
 
         public bool IsPowerOfX(int n, int x)
