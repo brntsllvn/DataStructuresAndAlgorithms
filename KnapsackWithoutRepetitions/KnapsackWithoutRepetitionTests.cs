f4e60fa91f5ee3e6fa112f992cc8d1c5f4321371
Brent Sullivan <brntsllvn@gmail.com>
2016-07-14 07:57:17 -0700

    Knapsack without repetitions class example infrastructure implemented

---
+++ KnapsackWithoutRepetitions/KnapsackWithoutRepetitionTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using Shouldly;
+
+namespace KnapsackWithoutRepetitions
+{
+    [TestFixture]
+    class KnapsackWithoutRepetitionTests
+    {
+        [Test]
+        public void ClassExample()
+        {
+            var knapsackCapacity = 10;
+            var potentialItemsValuesAndWeights = new int[,] {{30, 6}, {14, 3}, {16, 4}, {9, 2}};
+            var expectedItemsInKnapsack = new int[] {0, 1, 0, 1};
+
+            var f0 = new Launcher();
+            f0.KnapsackNoRep(knapsackCapacity, potentialItemsValuesAndWeights).ShouldBe(expectedItemsInKnapsack);
+
+            Should.CompleteIn(
+                () => f0.KnapsackNoRep(knapsackCapacity, potentialItemsValuesAndWeights), TimeSpan.FromMilliseconds(1500));
+
+            var proc = Process.GetCurrentProcess();
+            proc.PrivateMemorySize64.ShouldBeLessThanOrEqualTo(512 * 1000 * 1000);
+        }
+    }
+}
---
+++ KnapsackWithoutRepetitions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace KnapsackWithoutRepetitions
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Launcher().Run(args);
+        }
+    }
+
+    public class Launcher
+    {
+        public void Run(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+            //Console.WriteLine(KnapsackNoRep(input));
+        }
+
+        public int[] KnapsackNoRep(int knapsackCapacity, int[,] potentialItemsValuesAndWeights)
+        {
+            return new [] {0};
+        }
+    }
+}
