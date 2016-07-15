d6de31691f832ed846e6eca14ca59ec1e3381b67
Brent Sullivan <brntsllvn@gmail.com>
2016-07-15 08:49:02 -0700

    Knapsack gold initial solution

--- KnapsackWithoutRepetitions/KnapsackWithoutRepetitionTests.cs
+++ KnapsackWithoutRepetitions/KnapsackWithoutRepetitionTests.cs
@@ -8,19 +8,17 @@ namespace KnapsackWithoutRepetitions
     [TestFixture]
     class KnapsackWithoutRepetitionTests
     {
-        [Test]
-        public void ClassExample()
+        [TestCase("A", 10, new[] { 0, 6, 3, 4, 2 }, new[] { 0, 30, 14, 16, 9 }, 46 )]
+        [TestCase("B", 10, new[] { 0, 1, 4, 8 }, new[] { 0, 1, 4, 8 }, 9)]
+        public void KnapNoRep(string testCase, int knapsackCapacity, int[] weights, int[] values, int expectedValue)
         {
-            var knapsackCapacity = 10;
-            var potentialItemValuesAndWeights = new[,] { { 30, 6 }, { 14, 3 }, { 16, 4 }, { 9, 2 } };
-            //var expectedItemsInKnapsack = new[] { 1, 0, 1, 0 };
-            var expectedValue = 48;
+            weights.Length.ShouldBe(values.Length);
 
             var f0 = new Launcher();
-            f0.KnapsackNoRep(knapsackCapacity, potentialItemValuesAndWeights).ShouldBe(expectedValue);
+            f0.KnapsackNoRep(knapsackCapacity, weights, values).ShouldBe(expectedValue);
 
             Should.CompleteIn(
-                () => f0.KnapsackNoRep(knapsackCapacity, potentialItemValuesAndWeights), TimeSpan.FromMilliseconds(1500));
+                () => f0.KnapsackNoRep(knapsackCapacity, weights, values), TimeSpan.FromMilliseconds(1500));
 
             var proc = Process.GetCurrentProcess();
             proc.PrivateMemorySize64.ShouldBeLessThanOrEqualTo(512 * 1000 * 1000);
--- KnapsackWithoutRepetitions/Program.cs
+++ KnapsackWithoutRepetitions/Program.cs
@@ -15,35 +15,46 @@ namespace KnapsackWithoutRepetitions
     {
         public void Run(string[] args)
         {
-            var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
-            //Console.WriteLine(KnapsackNoRep(input));
+            var line1Input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+            var knapsackCapacity = line1Input[0];
+            var line2Input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+
+            var appendedLine2Input = new int[line2Input.Length + 1];
+            line2Input.CopyTo(appendedLine2Input,1);
+
+            var weights = appendedLine2Input;
+            var values = appendedLine2Input;
+            Console.WriteLine(KnapsackNoRep(knapsackCapacity, weights, values));
+            Console.Read();
         }
 
-        public int KnapsackNoRep(int knapsackCapacity, int[,] potentialItemValuesAndWeights)
+        public int KnapsackNoRep(int totalKnapsackCapacity, int[] weights, int[] values)
         {
-            var numberItems = potentialItemValuesAndWeights.GetLength(0);
-            var value = new int[knapsackCapacity, numberItems];
 
-            for (int i = 0; i < numberItems; i++)
+
+            var numItems = weights.GetLength(0);
+            var value = new int[totalKnapsackCapacity+1, numItems];
+
+            for (int itemNumber = 1; itemNumber < numItems; itemNumber++)
             {
-                for (int w = 1; w < knapsackCapacity; w++)
+                for (int knapsackCapacityIncrementer = 1; knapsackCapacityIncrementer <= totalKnapsackCapacity; knapsackCapacityIncrementer++)
                 {
-                    value[w, i] = value[w, i - 1];
+                    value[knapsackCapacityIncrementer, itemNumber] = value[knapsackCapacityIncrementer, itemNumber - 1];
 
-                    var potentialItemWeight = potentialItemValuesAndWeights[i, 1];
-                    if (potentialItemWeight < knapsackCapacity)
+                    var potentialItemWeight = weights[itemNumber];
+                    if (potentialItemWeight <= knapsackCapacityIncrementer)
                     {
-                        var potentialItemValue = potentialItemValuesAndWeights[i, 0];
-                        var tempValue = value[knapsackCapacity - w, i - 1] + potentialItemValue;
-                        if (value[w,i] < tempValue)
+                        var potentialItemValue = values[itemNumber];
+                        var tempValue = value[knapsackCapacityIncrementer - potentialItemWeight, itemNumber - 1] + potentialItemValue;
+                        if (value[knapsackCapacityIncrementer, itemNumber] < tempValue)
                         {
-                            value[w, i] = tempValue;
+                            value[knapsackCapacityIncrementer, itemNumber] = tempValue;
                         }
                     }
                 }
             }
 
-            return value[knapsackCapacity - 1, numberItems - 1];
+            return value[totalKnapsackCapacity, numItems-1];
         }
     }
 }
