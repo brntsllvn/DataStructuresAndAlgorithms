a1b63f9c15fb8eae94d96cd696e6338b0ed1b263
Brent Sullivan <brntsllvn@gmail.com>
2016-07-15 16:20:34 -0700

    final knapsack solution

--- KnapsackWithoutRepetitions/KnapsackWithoutRepetitionTests.cs
+++ KnapsackWithoutRepetitions/KnapsackWithoutRepetitionTests.cs
@@ -8,8 +8,11 @@ namespace KnapsackWithoutRepetitions
     [TestFixture]
     class KnapsackWithoutRepetitionTests
     {
+        [TestCase("AA",  99, new[] { 0, 100 }, new[] { 0, 100 }, 0 )]
+        [TestCase("AB", 100, new[] { 0, 100 }, new[] { 0, 100 }, 100 )]
         [TestCase("A", 10, new[] { 0, 6, 3, 4, 2 }, new[] { 0, 30, 14, 16, 9 }, 46 )]
         [TestCase("B", 10, new[] { 0, 1, 4, 8 }, new[] { 0, 1, 4, 8 }, 9)]
+        [TestCase("C", 1000, new[] { 0, 1000, 4, 8 }, new[] { 0, 1, 4, 8 }, 12)]
         public void KnapNoRep(string testCase, int knapsackCapacity, int[] weights, int[] values, int expectedValue)
         {
             weights.Length.ShouldBe(values.Length);
