34efd9680e41d40a8fda63600149c1c7843c37ce
Brent Sullivan <brntsllvn@gmail.com>
2016-07-18 13:22:54 -0700

    Tree Height problem

---
+++ TreeHeight/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TreeHeight
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Launcher().Run(args);
+        }
+    }
+
+    public class Launcher
+    {
+        public void Run(string[] args)
+        {
+            var numVertices = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
+            var parentCoordinates = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
+            Console.WriteLine(CalculateTreeHeight(parentCoordinates));
+        }
+
+        public string CalculateTreeHeight(long[] n)
+        {
+            return "hello";
+        }
+    }
+
+    public class BjsTree
+    {
+        public BjsNode Root { get; set; }
+    }
+
+    public class BjsNode
+    {
+        public string Payload { get; set; }
+        public List<BjsTree> ChildTrees { get; set; }
+
+        public BjsNode(string value)
+        {
+            Payload = value;
+        }
+    }
+}
---
+++ TreeHeight/TreeHeightTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Diagnostics;
+
+namespace TreeHeight
+{
+    [TestFixture]
+    class TreeHeightTests
+    {
+        [TestCase("A", new long[] { 0 }, "hello")]
+        public void Test_1(string caseName, long[] input, string expected)
+        {
+            var f0 = new Launcher();
+            f0.CalculateTreeHeight(input).ShouldBe(expected);
+
+            Should.CompleteIn(
+                () => f0.CalculateTreeHeight(input), TimeSpan.FromMilliseconds(1500));
+
+            var proc = Process.GetCurrentProcess();
+            proc.PrivateMemorySize64.ShouldBeLessThanOrEqualTo(512 * 1000 * 1000);
+        }
+    }
+}
