35e441786427b35f80921c11170739ab4a27decd
Brent Sullivan <brntsllvn@gmail.com>
2016-07-17 18:48:15 -0700

    Check Brackets solution with a couple failing cases

--- CheckBrackets/CheckBracketsTests.cs
+++ CheckBrackets/CheckBracketsTests.cs
@@ -8,25 +8,60 @@ namespace CheckBrackets
     [TestFixture]
     class CheckBracketsTests
     {
+        [TestCase("[", "1")]
+        [TestCase("}", "1")]
+        [TestCase("b", "1")]
         [TestCase("[]", "Success")]
-        //[TestCase("{", "1")]
-        //[TestCase("(", "1")]
-        //[TestCase("[", "1")]
-        //[TestCase("[\"\"]", "Success")]
-        //[TestCase("[\'\"]\'", "Success")]
-        public void Test_1(string input, string expected)
+        [TestCase("[}", "2")]
+        [TestCase("[](()", "3")]
+        [TestCase("[]()", "Success")]
+        [TestCase("([])", "Success")]
+        [TestCase("(())", "Success")]
+        [TestCase("([]!!!)?", "Success")]
+        [TestCase("foo(bar);", "Success")]
+        [TestCase("foo(bar[i);", "10")]
+        public void AAA_CheckingBrackets(string input, string expected)
         {
-            var sanitizedInput = input.ToCharArray();
-            //sanitizedInput.ShouldBe(new char[5] { '[', '\'', '"', ']', '\'' });
-
             var f0 = new Launcher();
-            f0.CheckBrackets(input).ShouldBe(expected);
+             f0.bracketsBalanced(input).ShouldBe(expected);
 
             Should.CompleteIn(
-                () => f0.CheckBrackets(input), TimeSpan.FromMilliseconds(1500));
+                () => f0.bracketsBalanced(input), TimeSpan.FromMilliseconds(1500));
 
             var proc = Process.GetCurrentProcess();
             proc.PrivateMemorySize64.ShouldBeLessThanOrEqualTo(512 * 1000 * 1000);
+        }
+
+        [Test]
+        public void MorePushing()
+        {
+            var myStack = new BjsStack();
+
+            myStack.Empty().ShouldBe(true);
+
+            myStack.Push('h');
+            myStack.Push('P');
+            myStack.Push('q');
+            myStack.Push('L');
+
+            myStack.Empty().ShouldBe(false);
+
+            myStack.TopNode().Payload.ShouldBe('L');
+            myStack.TopNode().Next.Payload.ShouldBe('q');
+
+            myStack.PopNode().Payload.ShouldBe('L');
+            myStack.TopNode().Payload.ShouldBe('q');
+            myStack.TopNode().Next.Payload.ShouldBe('P');
+
+            myStack.PopNode().Payload.ShouldBe('q');
+            myStack.TopNode().Payload.ShouldBe('P');
+            myStack.TopNode().Next.Payload.ShouldBe('h');
+
+            myStack.TopNode().Payload.ShouldBe('P');
+            myStack.PopNode().Payload.ShouldBe('P');
+            myStack.PopNode().Payload.ShouldBe('h');
+
+            myStack.Empty().ShouldBe(true);
         }
     }
 }
--- CheckBrackets/Program.cs
+++ CheckBrackets/Program.cs
@@ -17,14 +17,93 @@ namespace CheckBrackets
         {
             //var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
             var input = Console.ReadLine();
-            Console.WriteLine(CheckBrackets(input));
+
+            Console.WriteLine(bracketsBalanced(input));
         }
 
-        public string CheckBrackets(string input)
+        public string bracketsBalanced(string input)
         {
-            // design an algorithm that checks if every {,[,( has a matching },],)
+            if (input.Length == 1)
+                return "1";
+
+            var bracketStack = new BjsStack();
+
+            char[] openBracketChars = { '{', '[', '(' };
+            char[] closeBracketChars = { '}', ']', ')' };
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var c = input[i];
+
+                if (openBracketChars.Contains(c))
+                    bracketStack.Push(c);
+                else
+                {
+                    if (bracketStack.Empty())
+                        return (i + 1).ToString();
+
+                    if (!closeBracketChars.Contains(c))
+                        continue;
+
+                    var top = bracketStack.PopNode().Payload;
+
+                    if ((top == '[' && c != ']')
+                        || (top == '{' && c != '}')
+                        || (top == '(' && c != ')'))
+                    {
+                        return (i+1).ToString();
+                    }
+                }
+            }
 
             return "Success";
+        }
+    }
+
+    public class BjsStack
+    {
+        public BjsNode Top { get; set; }
+
+        public bool Empty()
+        {
+            if (Top == null)
+                return true;
+            return false;
+        }
+
+        public void Push(char value)
+        {
+            var newListNode = new BjsNode(value);
+
+            if (Top != null)
+                newListNode.Next = Top;
+
+            Top = newListNode;
+        }
+
+        public BjsNode TopNode()
+        {
+            return Top;
+        }
+
+        public BjsNode PopNode()
+        {
+            var topToPop = Top;
+            var newTop = Top.Next;
+            Top = newTop;
+
+            return topToPop;
+        }
+    }
+
+    public class BjsNode
+    {
+        public char Payload { get; set; }
+        public BjsNode Next { get; set; }
+
+        public BjsNode(char payload)
+        {
+            Payload = payload;
         }
     }
 }
