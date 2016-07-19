1703b65537c23029e10f42a9411902a8bcbc6f79
Brent Sullivan <brntsllvn@gmail.com>
2016-07-19 08:52:39 -0700

    Tree height wip: adding children to tree nodes

--- TreeHeight/Program.cs
+++ TreeHeight/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -18,77 +19,97 @@ namespace TreeHeight
         {
             var numVertices = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
             var parentCoordinates = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
-            Console.WriteLine(CalculateTreeHeight(parentCoordinates));
+            var tree = ConstructTreeFromArray(parentCoordinates);
+            Console.WriteLine(CalculateTreeHeight(tree));
         }
 
-        public int CalculateTreeHeight(int[] n)
+        private List<TreeNode> ConstructTreeFromArray(int[] parentCoordinates)
         {
-            if (n.Length == 0)
+            //  0 1 2 3 4
+            // -1 0 0 1 2
+
+            var numNodes = parentCoordinates.Length;
+
+            var tree = new List<TreeNode>();
+            for (int i = 0; i < numNodes; i++)
+                tree.Add(new TreeNode(i));
+
+            for (int j = 0; j < numNodes; j++)
+            {
+                // set parent
+                TreeNode parentNode = null;
+                if (parentCoordinates[j] > -1)
+                    parentNode = tree.ElementAt(parentCoordinates[j]);
+                tree[j].Parent = parentNode;
+
+                // set children
+                // return all nodes with parent = j
+                var childrenLocations = Enumerable
+                    .Range(0, parentCoordinates.Length)
+                    .Where(k => parentCoordinates[k] == j)
+                    .ToList();
+
+                foreach (var childLocation in childrenLocations)
+                {
+                    if (parentCoordinates[j] > -1)
+                        tree[j].Children.Add(j, tree.ElementAt(childLocation));
+                }
+            }
+
+            return tree;
+        }
+
+        public int CalculateTreeHeight(List<TreeNode> tree)
+        {
+            if (tree.Count == 0)
                 return 0;
 
-            if (n.Length == 1)
+            if (tree.Count == 1)
                 return 1;
 
-            // I don't like constructing the tree on every iteration...
-            var root = ConstructTree(n);
-
-            var subTreeHeights = new List<int>();
-
-            // start with root
-            // loop through each child and calculate height
-            foreach (var childNode in root.ChildNodes)
-            {
-                // need to trim tree to use recursion
-                subTreeHeights = CalculateTreeHeight();
-            }
-
-            // take max iteratively
-
-            // measure tree height
-            var treeHeight = 1; 
+            var treeHeight = -1; 
             return treeHeight;
-        }
-
-        public BjsNode ConstructTree(int[] n)
-        {
-            // construct tree nodes from array
-            var tree = new BjsNode[n.Length];
-            BjsNode root = null;
-            for (int i = 0; i < n.Length; i++)
-            {
-                tree[i] = new BjsNode(i) {Payload = i};
-                if (tree[i].Payload == -1)
-                    root = tree[i];
-                else
-                {
-                    // !!! this is wrong...but makes the code compile !!!
-                    root = tree[0]; 
-                }
-            }
-
-            // populate child associations
-            for (int j = 0; j < n.Length; j++)
-            {
-                for (int k = 0; k < n.Length; k++)
-                {
-                    if (k != j && tree[k].Payload == j)
-                    {
-                        tree[j].ChildNodes.Add(tree[k]);
-                    }
-                }
-            }
-            return root;
         }
     }
 
-    public class BjsNode
+    public class TreeNode : IEnumerable<TreeNode>
     {
-        public int Payload { get; set; }
-        public List<BjsNode> ChildNodes { get; set; }
+        public Dictionary<int, TreeNode> Children = new Dictionary<int, TreeNode>();
+        public int ID;
+        public TreeNode Parent { get; set; }
 
-        public BjsNode(int value)
+        public TreeNode(int id)
         {
-            Payload = value;
+            ID = id;
+        }
+
+        public TreeNode GetChild(int id)
+        {
+            return Children[id];
+        }
+
+        public void Add(TreeNode node)
+        {
+            if (node.Parent != null)
+                node.Parent.Children.Remove(node.ID);
+
+            node.Parent = this;
+            Children.Add(node.ID, node);
+        }
+
+        public IEnumerator<TreeNode> GetEnumerator()
+        {
+            return Children.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int Count
+        {
+            get { return Children.Count; }
         }
     }
 }
