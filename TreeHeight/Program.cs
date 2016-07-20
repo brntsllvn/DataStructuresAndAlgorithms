6bb60bb283c6f71700941aeaed7fe73d5336210e
Brent Sullivan <brntsllvn@gmail.com>
2016-07-20 14:00:50 -0700

    Successfully reduced array->tree to O(n) from O(n^2)

--- TreeHeight/Program.cs
+++ TreeHeight/Program.cs
@@ -1,113 +1,119 @@
-﻿//using System;
-//using System.Collections;
-//using System.Collections.Generic;
-//using System.Linq;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework.Constraints;
 
-//namespace TreeHeight
-//{
-//    public class Program
-//    {
-//        public static void Main(string[] args)
-//        {
-//            new Launcher().Run(args);
-//        }
-//    }
+namespace TreeHeight
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Launcher().Run(args);
+        }
+    }
 
-//    public class Launcher
-//    {
-//        public void Run(string[] args)
-//        {
-//            var numVertices = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
-//            var parentCoordinates = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
-//            var tree = ConstructTreeFromArray(parentCoordinates);
-//            var root = tree.FirstOrDefault(node => node.Parent == null);
-//            Console.WriteLine(CalculateTreeHeight(root));
-//        }
+    public class Launcher
+    {
+        public void Run(string[] args)
+        {
+            var numVertices = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+            var parentCoordinates = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+            var tree = ConstructTreeFromArray(parentCoordinates);
+            Console.WriteLine(CalculateTreeHeight(tree));
+        }
 
-//        public int CalculateTreeHeight(TreeNode node)
-//        {
-//            if (node == null)
-//                return 0;
+        public int CalculateTreeHeight(List<TreeNode> tree)
+        {
+            if (tree.Count == 0)
+                return 0;
 
-//            var q = new List<TreeNode>();
-//            q.Add(node);
+            if (tree.Count == 1)
+                return 1;
 
-//            int height = 0;
+            var root = tree.FirstOrDefault(node => node.Parent == null) ?? tree.FirstOrDefault().Parent;
 
-//            while (true)
-//            {
-//                int nodeCount = q.Count;
-//                if (nodeCount == 0)
-//                    return height;
+            var rootChildren = root.Children;
 
-//                height++;
+            var childrenTreeHeights = new List<int>();
 
-//                while (nodeCount > 0)
-//                {
-//                    var newnode = q.First();
-//                    q.RemoveAt(0);
+            foreach (var child in rootChildren)
+            {
+                var grandChildren = child.Value.GetAllChildren();
+                grandChildren.Add(child.Value);
 
-//                    q.AddRange(newnode.GetAllChildren());
+                var height = CalculateTreeHeight(grandChildren);
+                childrenTreeHeights.Add(height);
+            }
 
-//                    nodeCount--;
-//                }
-//            }
-//        }
+            return 1 + childrenTreeHeights.Max();
+        }
 
-//        public List<TreeNode> ConstructTreeFromArray(int[] parentCoordinates)
-//        {
-//            var numNodes = parentCoordinates.Length;
+        public List<TreeNode> ConstructTreeFromArray(int[] parentCoordinates)
+        {
+            var numNodes = parentCoordinates.Length;
 
-//            var tree = new List<TreeNode>();
-//            for (int i = 0; i < numNodes; i++)
-//                tree.Add(new TreeNode(i) {Parent = new TreeNode(parentCoordinates[i])});
+            var tree = new List<TreeNode>();
+            for (int i = 0; i < numNodes; i++)
+                tree.Add(new TreeNode(i));
 
-//            if (parentCoordinates.Length == 1)
-//                return tree;
+            for (int j = 0; j < numNodes; j++)
+            {
+                if (parentCoordinates[j] != -1)
+                {
+                    tree[j].Parent = tree[parentCoordinates[j]];
+                    tree[parentCoordinates[j]].Children.Add(j, tree[j]);
+                }
+            }
 
-//            //for (int j = 0; j < numNodes; j++)
-//            //{
-//            //    TreeNode parentNode = null;
-//            //    if (parentCoordinates[j] > -1)
-//            //        parentNode = tree.ElementAt(parentCoordinates[j]);
-//            //    tree[j].Parent = parentNode;
+            return tree;
+        }
+    }
 
-//            //    var childrenLocations = Enumerable
-//            //        .Range(0, parentCoordinates.Length)
-//            //        .Where(k => parentCoordinates[k] == j)
-//            //        .ToList();
+    public class TreeNode : IEnumerable<TreeNode>
+    {
+        public Dictionary<int, TreeNode> Children = new Dictionary<int, TreeNode>();
+        public int ID;
+        public TreeNode Parent { get; set; }
 
-//            //    foreach (var childLocation in childrenLocations)
-//            //        tree[j].Children.Add(childLocation, tree.ElementAt(childLocation));
-//            //}
+        public TreeNode(int id)
+        {
+            ID = id;
+        }
 
-//            return tree;
-//        }
-//    }
+        public TreeNode GetChild(int id)
+        {
+            return Children[id];
+        }
 
-//    public class TreeNode
-//    {
-//        public Dictionary<int, TreeNode> Children = new Dictionary<int, TreeNode>();
-//        public int Id;
-//        public TreeNode Parent { get; set; }
+        public List<TreeNode> GetAllChildren()
+        {
+            return Children.Values.ToList();
+        }
 
-//        public TreeNode(int id)
-//        {
-//            Id = id;
-//        }
+        public void Add(TreeNode node)
+        {
+            if (node.Parent != null)
+                node.Parent.Children.Remove(node.ID);
 
-//        public List<TreeNode> GetAllChildren()
-//        {
-//            return Children.Values.ToList();
-//        }
+            node.Parent = this;
+            Children.Add(node.ID, node);
+        }
 
-//        public void Add(TreeNode node)
-//        {
-//            if (node.Parent != null)
-//                node.Parent.Children.Remove(node.Id);
+        public IEnumerator<TreeNode> GetEnumerator()
+        {
+            return Children.Values.GetEnumerator();
+        }
 
-//            node.Parent = this;
-//            Children.Add(node.Id, node);
-//        }
-//    }
-//}
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        //public int CountChildren
+        //{
+        //    get { return Children.Count; }
+        //}
+    }
+}
--- TreeHeight/TreeHeight.cs
+++ TreeHeight/TreeHeight.cs
@@ -1,76 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
 
-namespace TreeHeight
-{
-    internal class TreeHeight
-    {
-        public int N { get; set; }
-        public int[] Parent { get; set; }
+//namespace TreeHeight
+//{
+//    internal class TreeHeight
+//    {
+//        public int N { get; set; }
+//        public int[] Parent { get; set; }
 
-        public TreeHeight(int n, int[] parent)
-        {
-            N = n;
-            Parent = parent;
-        }
+//        public TreeHeight(int n, int[] parent)
+//        {
+//            N = n;
+//            Parent = parent;
+//        }
 
-        public int CalculateTreeHeight()
-        {
-            if (Parent.Length == 0)
-                return 0;
+//        public int CalculateTreeHeight()
+//        {
+//            if (Parent.Length == 0)
+//                return 0;
 
-            var q = new List<int>();
-            q.Add(Array.IndexOf(Parent,-1));
+//            var q = new List<int>();
+//            q.Add(Array.IndexOf(Parent,-1));
 
-            int height = 0;
+//            int height = 0;
 
-            while (true)
-            {
-                int nodeCount = q.Count;
-                if (nodeCount == 0)
-                    return height;
+//            while (true)
+//            {
+//                int nodeCount = q.Count;
+//                if (nodeCount == 0)
+//                    return height;
 
-                height++;
+//                height++;
 
-                while (nodeCount > 0)
-                {
-                    var newnode = q.First();
-                    q.RemoveAt(0);
+//                while (nodeCount > 0)
+//                {
+//                    var newnode = q.First();
+//                    q.RemoveAt(0);
 
-                    q.AddRange(GetChildren(newnode));
+//                    q.AddRange(GetChildren(newnode));
 
-                    nodeCount--;
-                }
-            }
-        }
+//                    nodeCount--;
+//                }
+//            }
+//        }
 
-        public List<int> GetChildren(int index)
-        {
-            var childrenLocations = Enumerable
-                .Range(0, Parent.Length)
-                .Where(k => Parent[k] == index)
-                .ToList();
-            return childrenLocations;
-        } 
-    }
-}
+//        public List<int> GetChildren(int index)
+//        {
+//            var childrenLocations = Enumerable
+//                .Range(0, Parent.Length)
+//                .Where(k => Parent[k] == index)
+//                .ToList();
+//            return childrenLocations;
+//        } 
+//    }
+//}
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        new Launcher().Run(args);
-    }
-}
+//public class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//        new Launcher().Run(args);
+//    }
+//}
 
-public class Launcher
-{
-    public void Run(string[] args)
-    {
-        var numVertices = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray()[0];
-        var parentCoordinates = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
-        var tree = new TreeHeight.TreeHeight(numVertices, parentCoordinates);
-        Console.WriteLine(tree.CalculateTreeHeight());
-    }
-}
+//public class Launcher
+//{
+//    public void Run(string[] args)
+//    {
+//        var numVertices = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray()[0];
+//        var parentCoordinates = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+//        var tree = new TreeHeight.TreeHeight(numVertices, parentCoordinates);
+//        Console.WriteLine(tree.CalculateTreeHeight());
+//    }
+//}
