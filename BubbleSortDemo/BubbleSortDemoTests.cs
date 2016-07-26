5427f616106398856611b84d578600a0633d8bf7
Brent Sullivan <brntsllvn@gmail.com>
2016-07-25 18:07:11 -0700

    bubble sort demo

---
+++ BubbleSortDemo/BubbleSortDemoTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace BubbleSortDemo
+
+{
+    [TestFixture]
+    class BubbleSortDemoTests
+    {
+        [TestCase("A", new int[] { 0 }, new int[] { 0 })]
+        [TestCase("B", new int[] { 0, 1 }, new int[] { 0, 1 })]
+        [TestCase("C", new int[] { 0, 1, -1 }, new int[] { -1, 0, 1 })]
+        [TestCase("D", new int[] { 5, 1, -1, 0 }, new int[] { -1, 0, 1, 5 })]
+        public void Test_1(string caseName, int[] input, int[] expected)
+        {
+            var f0 = new Launcher();
+            f0.BubbleSort(input);
+            input.ShouldBe(expected);
+        }
+    }
+}
---
+++ BubbleSortDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace BubbleSortDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Launcher
+    {
+        public void BubbleSort(int[] n)
+        {
+            for (int i = 0; i < n.Length; i++)
+            {
+                for (int j = 0; j < n.Length - 1; j++)
+                {
+                    if (n[j] > n[j+1])
+                    {
+                        var temp = n[j];
+                        n[j] = n[j + 1];
+                        n[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
