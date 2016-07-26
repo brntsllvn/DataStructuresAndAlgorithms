9d25abbe66525550e0c62eb224e4043aee935116
Brent Sullivan <brntsllvn@gmail.com>
2016-07-25 18:10:29 -0700

    Project setup for array-to-heap

---
+++ ArrayToHeap/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayToHeap
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Launcher().Run(args);
+        }
+    }
+
+    public class Launcher
+    {
+        public void Run(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
+            Console.WriteLine(MyFunction(input));
+        }
+
+        public string MyFunction(long[] n)
+        {
+            return "hello";
+        }
+    }
+}
---
+++ LauncherTemplate/ArrayToHeapTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Diagnostics;
+
+namespace LauncherTemplate
+{
+    [TestFixture]
+    class ArrayToHeapTests
+    {
+        [TestCase("A", new long[] { 0 }, "hello")]
+        public void Test_1(string caseName, long[] input, string expected)
+        {
+            var f0 = new Launcher();
+            f0.MyFunction(input).ShouldBe(expected);
+
+            Should.CompleteIn(
+                () => f0.MyFunction(input), TimeSpan.FromMilliseconds(1500));
+
+            var proc = Process.GetCurrentProcess();
+            proc.PrivateMemorySize64.ShouldBeLessThanOrEqualTo(512 * 1000 * 1000);
+        }
+    }
+}
