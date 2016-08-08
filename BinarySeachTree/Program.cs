7e06dae71fbe5ee2e20093d1335c7dd01aa08d70
Brent Sullivan <brntsllvn@gmail.com>
2016-08-08 07:46:12 -0700

    Binary search tree problem set up

---
+++ BinarySeachTree/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySeachTree
+{
+    public class Program
+    {
+        public string MagicFunctionThatSolvesAllProblems()
+        {
+            return "hello";
+        }
+
+        public void ReadData()
+        {
+            var input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
+        }
+
+        public void WriteResponse(string result)
+        {
+            Console.WriteLine(result);
+        }
+
+        public void Run()
+        {
+            ReadData();
+            var result = MagicFunctionThatSolvesAllProblems();
+            WriteResponse(result);
+        }
+
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+    }
+}
---
+++ BinarySeachTree/aaBinarySearchTreeTests.cs
@@ -0,0 +1,42 @@
+﻿using BinarySeachTree;
+using NUnit.Framework;
+using Shouldly;
+
+namespace BinarySearchTree
+{
+    [TestFixture]
+    class aaBinarySearchTreeTests
+    {
+        [TestCase("A", new long[] { 0 }, "hello")]
+        public void Test_1(string caseName, long[] input, string expected)
+        {
+            var f0 = new Program();
+            var result = f0.MagicFunctionThatSolvesAllProblems();
+            result.ShouldBe(expected);
+        }
+    }
+
+    //[TestFixture]
+    //class PhoneBookTests
+    //{
+    //    [TestCase("A", "hi", "hi")]
+    //    public void PhoneBook_Cases(string caseName, string input, string expected)
+    //    {
+    //        input.ShouldBe(expected);
+    //    }
+
+    //    [Test, TestCaseSource(nameof(ResultList))]
+    //    public void A_Test(string caseName, List<InputTriple> input)
+    //    {
+    //        var firstInput = input[0];
+    //        firstInput.Command.ShouldBe("add");
+    //        firstInput.Number.ShouldBe("911");
+    //        firstInput.Person.ShouldBe("police");
+    //    }
+
+    //    private static readonly object[] ResultList =
+    //    {
+    //        new object[] { "A", new List<InputTriple> { new InputTriple("add","911","police") } },
+    //    };
+    //}
+}
