c75a5d335f086b555e903f47b709d5c57bd446b5
Brent Sullivan <brntsllvn@gmail.com>
2016-08-10 10:42:20 -0700

    New project: Set With Range Sums

---
+++ SetWithRangeSums/aaaSetWithRangeSums.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace SetWithRangeSums
+{
+    [TestFixture]
+    class aaaSetWithRangeSums
+    {
+        [TestCase("A", new long[] { 0 }, "hello")]
+        public void Test_1(string caseName, long[] input, string expected)
+        {
+            var f0 = new Program();
+            var result = f0.MagicFunctionThatSolvesAllProblems();
+            result.ShouldBe(expected);
+        }
+    }
+}
